87f832853a0513fcc40921a4d5b1214fdc9c7708
David Dal Busco <david.dalbusco@outlook.com>
2022-10-16 12:29:38 +0200

    feat: update_settings for controllers

--- src/rust_demo_backend/src/lib.rs
+++ src/rust_demo_backend/src/lib.rs
@@ -1,11 +1,10 @@
 use ic_cdk::api::management_canister::main::*;
-use ic_cdk::{api};
+use ic_cdk::{ api };
 
 #[ic_cdk_macros::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
-
 
 // https://github.com/dfinity/cdk-rs/tree/main/examples/management_canister
 
@@ -21,18 +20,20 @@ async fn create_bucket(user_id: ic_cdk::export::Principal) -> ic_cdk::export::Pr
             freezing_threshold: None,
         }),
     };
-    create_canister(arg).await.unwrap();
 
-    let canister_id = create_canister_with_extra_cycles(
-        CreateCanisterArgument::default(),
-        1_000_000_000_000u128,
-    )
-    .await
-    .unwrap()
-    .0
-    .canister_id;
+    let canister_id = create_canister(arg).await.unwrap().0.canister_id;
 
-    // TODO: update settings with caller and user_id caller, user_id
+    let arg = UpdateSettingsArgument {
+        canister_id,
+        settings: CanisterSettings {
+            controllers: Some(vec![ic_cdk::id(), caller, user_id]),
+            compute_allocation: None,
+            memory_allocation: None,
+            freezing_threshold: None,
+        },
+    };    
+
+    update_settings(arg).await.unwrap();
 
     return canister_id;
 }
