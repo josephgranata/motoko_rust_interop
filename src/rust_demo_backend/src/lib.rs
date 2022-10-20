0f76f26f0cbce60356b716fc3749f59f3d491768
David Dal Busco <david.dalbusco@outlook.com>
2022-10-20 12:09:31 +0200

    feat: init rust canister owner

--- src/rust_demo_backend/src/lib.rs
+++ src/rust_demo_backend/src/lib.rs
@@ -2,15 +2,42 @@ use ic_cdk::api::management_canister::main::{ canister_status, CanisterIdRecord,
 use ic_cdk::{ api };
 use candid::{ Nat, Principal };
 use ic_cdk_macros::{ init, query, update };
+use std::cell::RefCell;
+
+// https://medium.com/encode-club/encode-x-internet-computer-intro-to-building-on-the-ic-in-rust-video-slides-b496d6baad08
+// https://github.com/hpeebles/rust-canister-demo/blob/master/todo/src/lib.rs
+
+thread_local! {
+    static STATE: RefCell<State> = RefCell::default();
+}
+
+#[derive(Default)]
+struct State {
+    owner: Option<Principal>,
+}
 
 #[init]
 fn init(user: Principal) {
     ic_cdk::print(format!("Initializing bucket., {}", user.to_text()));
+    STATE.with(|state| {
+        *state.borrow_mut() = State {
+            owner: Some(user),
+        };
+    });
 }
 
 #[query]
 fn greet(name: String) -> String {
-    format!("Hello super super, {}!", name)
+    let owner: Option<Principal> = STATE.with(|state| get_owner(&state.borrow()));
+
+    match owner {
+        Some(owner) => format!("Hello super, {}, {o}!", name, o = owner.to_text()),
+        None => String::from("No owner."),
+    }
+}
+
+fn get_owner(state: &State) -> Option<Principal> {
+    state.owner
 }
 
 #[update]
