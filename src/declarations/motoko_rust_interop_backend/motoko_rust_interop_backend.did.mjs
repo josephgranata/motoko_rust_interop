5bf0f8efd467971b6c2d0d8f706eb67efc7eef10
David Dal Busco <david.dalbusco@outlook.com>
2022-10-20 08:41:26 +0200

    feat: add mjs

---
+++ src/declarations/motoko_rust_interop_backend/motoko_rust_interop_backend.did.mjs
@@ -0,0 +1,19 @@
+export const idlFactory = ({ IDL }) => {
+  return IDL.Service({
+    'delete' : IDL.Func([], [IDL.Principal], []),
+    'init' : IDL.Func([], [IDL.Principal], []),
+    'installCode' : IDL.Func(
+        [IDL.Principal, IDL.Vec(IDL.Nat8), IDL.Vec(IDL.Nat8)],
+        [],
+        [],
+      ),
+    'storageLoadWasm' : IDL.Func(
+        [IDL.Vec(IDL.Nat8)],
+        [IDL.Record({ 'total' : IDL.Nat, 'chunks' : IDL.Nat })],
+        [],
+      ),
+    'storateResetWasm' : IDL.Func([IDL.Vec(IDL.Nat8)], [], []),
+    'wasmHash' : IDL.Func([], [IDL.Vec(IDL.Nat8)], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
