8481b73a2454f643010aee0f1df48e4b04347f76
David Dal Busco <david.dalbusco@outlook.com>
2022-10-22 10:02:16 +0200

    feat: create chunk

--- src/rust_demo_backend/src/store.rs
+++ src/rust_demo_backend/src/store.rs
@@ -1,28 +1,67 @@
 use ic_cdk::{ api };
 
 use crate::{ STATE, types::{ AssetKey, Batch, State } };
+use crate::types::Chunk;
 
 // Upload batch and chunks
 
 const BATCH_EXPIRY_NANOS: u64 = 300_000_000_000;
 
-static mut next_back_id: u8 = 0;
+static mut NEXT_BACK_ID: u8 = 0;
+static mut NEXT_CHUNK_ID: u8 = 0;
 
 pub fn create_batch(key: AssetKey) -> u8 {
     STATE.with(|state| create_batch_impl(key, &mut state.borrow_mut()))
+}
+
+pub fn create_chunk(chunk: Chunk) -> Result<u8, &'static str> {
+    STATE.with(|state| create_chunk_impl(chunk, &mut state.borrow_mut()))
 }
 
 fn create_batch_impl(key: AssetKey, state: &mut State) -> u8 {
     let now = api::time();
 
     unsafe {
-        next_back_id = next_back_id + 1;
+        NEXT_BACK_ID = NEXT_BACK_ID + 1;
 
-        state.batches.insert(next_back_id, Batch {
+        state.batches.insert(NEXT_BACK_ID, Batch {
             key,
             expiresAt: now + BATCH_EXPIRY_NANOS,
         });
 
-        next_back_id
+        NEXT_BACK_ID
+    }
+}
+
+fn create_chunk_impl(Chunk {batchId, content}: Chunk, state: &mut State) -> Result<u8, &'static str> {
+    let batch = state.batches.get(&batchId);
+
+    match batch {
+        None => Err("Batch not found."),
+        Some(b) => {
+            let now = api::time();
+
+            state.batches.insert(
+                batchId,
+                Batch {
+                    key: batch.key,
+                    expiresAt: now + BATCH_EXPIRY_NANOS
+                }
+            );
+
+            unsafe {
+                NEXT_CHUNK_ID = NEXT_CHUNK_ID + 1;
+
+                state.chunks.insert(
+                    NEXT_CHUNK_ID,
+                    Chunk {
+                        batchId,
+                        content
+                    }
+                );
+
+                Ok(NEXT_CHUNK_ID)
+            }
+        }
     }
 }
