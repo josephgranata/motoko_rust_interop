38a7874531f284913390f8aa4cf74dd71a9c7065
David Dal Busco <david.dalbusco@outlook.com>
2022-10-23 08:27:55 +0200

    feat: did files

--- src/rust_demo_backend/src/lib.rs
+++ src/rust_demo_backend/src/lib.rs
@@ -2,15 +2,16 @@ mod types;
 mod store;
 
 use ic_cdk::api::management_canister::main::{CanisterIdRecord, deposit_cycles};
+use ic_cdk_macros::{init, update, pre_upgrade, post_upgrade, query};
 use ic_cdk::api::{canister_balance128, caller, trap};
+use ic_cdk::export::candid::{candid_method,export_service};
 use ic_cdk::{storage, print};
 use candid::{Principal};
-use ic_cdk_macros::{init, query, update, pre_upgrade, post_upgrade};
 use std::cell::RefCell;
 use std::collections::HashMap;
 
 use crate::store::{commit_batch, create_batch, create_chunk};
-use crate::types::{interface::{InitUpload, UploadChunk,CommitBatch}, storage::{AssetKey, State, Chunk}};
+use crate::types::{interface::{InitUpload, UploadChunk, CommitBatch}, storage::{AssetKey, State, Chunk}};
 
 // https://medium.com/encode-club/encode-x-internet-computer-intro-to-building-on-the-ic-in-rust-video-slides-b496d6baad08
 // https://github.com/hpeebles/rust-canister-demo/blob/master/todo/src/lib.rs
@@ -51,25 +52,12 @@ fn post_upgrade() {
     // });
 }
 
-#[query]
-fn greet(name: String) -> String {
-    let owner: Option<Principal> = STATE.with(|state| get_owner(&state.borrow()));
-
-    match owner {
-        Some(owner) => format!("Hello super, {}, {o}!", name, o = owner.to_text()),
-        None => String::from("No owner."),
-    }
-}
-
-fn get_owner(state: &State) -> Option<Principal> {
-    state.user
-}
-
 //
 // Upload
 //
 
 #[allow(non_snake_case)]
+#[candid_method(update)]
 #[update]
 async fn initUpload(key: AssetKey) -> InitUpload {
     // TODO: is caller === user
@@ -79,6 +67,7 @@ async fn initUpload(key: AssetKey) -> InitUpload {
 }
 
 #[allow(non_snake_case)]
+#[candid_method(update)]
 #[update]
 async fn uploadChunk(chunk: Chunk) -> UploadChunk {
     // TODO: is caller === user
@@ -92,6 +81,7 @@ async fn uploadChunk(chunk: Chunk) -> UploadChunk {
 }
 
 #[allow(non_snake_case)]
+#[candid_method(update)]
 #[update]
 async fn commitUpload(commit: CommitBatch) {
     // TODO: is caller === user
@@ -108,8 +98,10 @@ async fn commitUpload(commit: CommitBatch) {
 // Canister mgmt
 //
 
+#[allow(non_snake_case)]
+#[candid_method(update)]
 #[update]
-async fn transfer_cycles() {
+async fn transferFreezingThresholdCycles() {
     let caller = caller();
 
     // TODO: is caller === manager
@@ -125,5 +117,29 @@ async fn transfer_cycles() {
     if cycles > 0 {
         let arg_deposit = CanisterIdRecord { canister_id: caller };
         deposit_cycles(arg_deposit, cycles).await.unwrap();
+    }
+}
+
+// Generate did files
+
+#[query(name = "__get_candid_interface_tmp_hack")]
+fn export_candid() -> String {
+    export_service!();
+    __export_service()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn save_candid() {
+        use std::env;
+        use std::fs::write;
+        use std::path::PathBuf;
+
+        let dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let dir = dir.parent().unwrap().parent().unwrap().join("src").join("rust_demo_backend");
+        write(dir.join("rust_demo_backend.did"), export_candid()).expect("Write failed.");
     }
 }
